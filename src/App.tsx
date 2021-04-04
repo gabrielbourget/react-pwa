d884bb735cd2a8035c989d472077f87516112d0d
Gabriel Bourget <gaboxa@syntronic.com>
2021-04-03 20:38:18 -0400

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar bg="primary" variant="dark">
+        <Navbar.Brand>Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
