dd80a68bb98cecd57e04659d461345b41137473e
Gabriel Bourget <gaboxa@syntronic.com>
2021-04-03 21:55:09 -0400

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+// import React from 'react';
 import './App.css';
 import { Navbar, Nav } from "react-bootstrap";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Users from "./Users/Users";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand>Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Navbar>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand>Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/users" exact component={Users} />
+        </Switch>
+      </Router>
     </div>
   );
 }
